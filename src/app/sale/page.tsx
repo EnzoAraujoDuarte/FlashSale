054d880066ad58dad0095464947f4a0effb31cec
Enzo Araujo <140770206+EnzoAraujoDuarte@users.noreply.github.com>
2026-03-15 01:08:56 -0300

    Update revalidation strategy for SalePage

--- src/app/sale/page.tsx
+++ src/app/sale/page.tsx
@@ -1,7 +1,7 @@
 import { DirectSalePage } from "@/components/ui/DirectSalePage";
-import { getCatalogProducts } from "@/lib/data/products";
+import { CATALOG_REVALIDATE_SECONDS, getCatalogProducts } from "@/lib/data/products";
 
-export const dynamic = "force-dynamic";
+export const revalidate = CATALOG_REVALIDATE_SECONDS;
 
 export default async function SalePage() {
   const initialProducts = await getCatalogProducts();
